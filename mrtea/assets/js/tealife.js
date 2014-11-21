74426a06cd39c3452c5a512be66053aa88eca9d7
codemadao <codemadao@users.noreply.github.com>
2014-11-21 19:13:00 +0100

    tabindex til alle html greiene, plus spoon/doob easter-egg

---
+++ mrtea/assets/js/tealife.js
@@ -0,0 +1,4 @@
+function tea(){
+	document.getElementById("teaLife").src = "assets/images/tea-doob.jpg" ;
+	document.getElementById("teaLife").alt = "Some like to smoke their herbs - Tea Life!" ;
+	}
